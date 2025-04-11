04494ad5d8ae2f9cf3f26d7681906036cf0bc6fd
SoReGaShi <folloutbabohead159@yahoo.co.jp>
2025-04-12 02:35:30 +0900

    Update 1-CreateSegmentsNoDemucs.py

--- 1-CreateSegmentsNoDemucs.py
+++ 1-CreateSegmentsNoDemucs.py
@@ -4,7 +4,7 @@ print("Running Aivis create-segments --no-use-demucs...")
 
 try:
     # Aivis create-segments コマンドを実行
-    !python -m Aivis create-segments --no-use-demucs
+    python -m Aivis create-segments --no-use-demucs
     print("[INFO] Aivis create-segmentsが完了しました。")
 except Exception as e:
     print(f"[ERROR] エラーが発生しました: {e}")
