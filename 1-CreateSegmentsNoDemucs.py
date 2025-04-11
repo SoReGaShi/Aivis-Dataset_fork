0b9b10c930401c688205ba8366d4c05986b7e6a4
SoReGaShi <folloutbabohead159@yahoo.co.jp>
2025-04-12 02:48:44 +0900

    Update 1-CreateSegmentsNoDemucs.py

--- 1-CreateSegmentsNoDemucs.py
+++ 1-CreateSegmentsNoDemucs.py
@@ -1,10 +1,12 @@
 # Colabで実行するために独自に追加したコードです
 
+import subprocess
+
 print("Running Aivis create-segments --no-use-demucs...")
 
 try:
     # Aivis create-segments コマンドを実行
-    python -m Aivis create-segments --no-use-demucs
+    subprocess.run(["python", "-m", "Aivis", "create-segments", "--no-use-demucs"], check=True)
     print("[INFO] Aivis create-segmentsが完了しました。")
 except Exception as e:
     print(f"[ERROR] エラーが発生しました: {e}")
