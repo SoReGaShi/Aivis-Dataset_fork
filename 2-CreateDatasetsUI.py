ff3b51e59030b7946f98c7126e963e544c3b7e2c
SoReGaShi <folloutbabohead159@yahoo.co.jp>
2025-04-12 02:10:14 +0900

    Create 2-CreateDatasetsUI.py

---
+++ 2-CreateDatasetsUI.py
@@ -0,0 +1,11 @@
+
+print("話者名を入力してください（複数の場合は空白含まない`,`区切り）")
+speaker_names = input("話者名: ")
+print("Running Aivis create-datasets...")
+
+try:
+   # Aivis create-datasets コマンドを実行
+   !python -m Aivis create-datasets ALL "{speaker_names}"
+   print("[INFO] Aivis create-datasetsが完了しました。")
+except Exception as e:
+   print(f"[ERROR] エラーが発生しました: {e}")
